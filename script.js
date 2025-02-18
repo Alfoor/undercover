1d81540fe0448d0774596cd0dc5e98a3b3b7951b
Alfoor <liamzab123@gmail.com>
2025-02-18 15:14:18 +0200

    Catan
    Like Catan

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scroll for in-page navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetElement = document.querySelector(this.getAttribute("href"));
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: "smooth"
+        });
+      }
+    });
+  });
+  
