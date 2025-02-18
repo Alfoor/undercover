694a93a6aad803d62b5f17a26f292b5685cd9378
Alfoor <liamzab123@gmail.com>
2025-02-18 15:18:55 +0200

    Catan 2

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
   
+  // Change navbar style on scroll
+  window.addEventListener('scroll', () => {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+  
